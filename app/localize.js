79f190a7f7ca1ab44078150fecadc0d211ea5c41
Dan Covill <daniel.covill@gmail.com>
2019-10-04 22:50:57 -0600

    Fix localization Add Netherlands translations

--- app/localize.js
+++ app/localize.js
@@ -1,9 +1,20 @@
+document.querySelectorAll('[data-localize-field]').forEach(element => {
+	let contentId = element.getAttribute('data-localize-value');
+	let fieldToLocalize = element.getAttribute('data-localize-field');
+	let message = chrome.i18n.getMessage(contentId);
+	if (!!message) {
+		element.setAttribute(fieldToLocalize, contentId);
+	} else {
+		console.error("Translation error");
+	}
+});
+// This way is redundant, but makes the HTML cleaner because it's what I'm doing most of the time
 document.querySelectorAll('[data-localize]').forEach(element => {
-    let contentId = element.getAttribute('data-localize');
-    let message = chrome.i18n.getMessage(contentId);
-    if(!!message) {
-        element.innerHTML = message;
-    } else {
-        element.innerHTML = "TRANSLATION ERROR";
-    }
+	let contentId = element.getAttribute('data-localize');
+	let message = chrome.i18n.getMessage(contentId);
+	if (!!message) {
+		element.innerHTML = message;
+	} else {
+		console.error("Translation error");
+	}
 });
